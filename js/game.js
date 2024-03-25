96bb18ac00dba9e6926c4c655722f863b638c5d1
Kevin Mueller <kevin.mueller@tuta.com>
2024-03-25 08:57:32 +0100

    added landscape check and turning img

--- js/game.js
+++ js/game.js
@@ -2,52 +2,62 @@ let canvas;
 let world;
 let keyboard = new Keyboard()
 let bgSound = new Audio('./audio/halloween-bg.mp3');
-let startGame = false; 
+let startGame = false;
 
+// Listen for resize changes
+window.addEventListener("resize", function () {
+    let orientationInfo = document.getElementById('landscape');
+    // Get screen size (inner/outerWidth, inner/outerHeight)
+    if (window.innerWidth > window.innerHeight && window.innerHeight < 500) {
+        orientationInfo.classList.add('d-none');
+    } else{
+        orientationInfo.classList.remove('d-none');
+    }
+}, false);
 
 function init() {
     canvas = document.getElementById('canvas');
 }
 
-function gameStart(){
+function gameStart() {
     startGame = true;
     document.getElementById('startscreen').style.display = 'none';
     document.getElementById('fullscreen-option').style.display = 'block';
     world = new World(canvas, keyboard);
 }
 
-function toggleAudio(){
-    if (startGame == false && bgSound.volume== 0) {
-        bgSound.volume =1;
+function toggleAudio() {
+    if (startGame == false && bgSound.volume == 0) {
+        bgSound.volume = 1;
         document.getElementById('sound-option').src = './img/menu/play-sound.png'
         return
-    } else if(startGame == false && bgSound.volume == 1){
-        bgSound.volume =0;
+    } else if (startGame == false && bgSound.volume == 1) {
+        bgSound.volume = 0;
         document.getElementById('sound-option').src = './img/menu/mute-sound.png'
         return
     }
-    if (world.volume== 0 ) {
+    if (world.volume == 0) {
         world.volume = 1;
         document.getElementById('sound-option').src = './img/menu/play-sound.png'
-    }else{
+    } else {
         world.volume = 0;
         document.getElementById('sound-option').src = './img/menu/mute-sound.png'
     }
 }
 
-function toggleFullscreen(){
-    if(canvas.webkitRequestFullScreen) {
+function toggleFullscreen() {
+    if (canvas.webkitRequestFullScreen) {
         canvas.webkitRequestFullScreen();
     }
-   else {
-      canvas.mozRequestFullScreen();
-   }     
+    else {
+        canvas.mozRequestFullScreen();
+    }
 }
 
 
 window.addEventListener('click', (event) => {
     if (event) {
-        if (startGame == true) 
+        if (startGame == true)
             bgSound.volume = world.volume;
         bgSound.play();
         bgSound.onended = (() => {
