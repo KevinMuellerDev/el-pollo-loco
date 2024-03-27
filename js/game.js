a48d92fdb08355b233ae2de4f099bd3c3fbb45ea
Kevin Müller <kevin.mueller@tuta.com>
2024-03-27 10:23:39 +0100

    added mobile control

--- js/game.js
+++ js/game.js
@@ -25,6 +25,8 @@ function checkOrientation() {
 function init() {
     canvas = document.getElementById('canvas');
     bgSound.volume = 0;
+    touchControlLeftHand();
+    touchControlRightHand();
 }
 
 
@@ -94,30 +96,56 @@ function slideIn() {
 }
 
 
+function touchControlLeftHand(){
+    document.getElementById('button-left').addEventListener('touchstart', (e)=>{
+        e.preventDefault();
+        keyboard.LEFT = true;
+    });
+    document.getElementById('button-left').addEventListener('touchend', (e)=>{
+        e.preventDefault();
+        keyboard.LEFT = false;
+    });
+    document.getElementById('button-right').addEventListener('touchstart', (e)=>{
+        e.preventDefault();
+        keyboard.RIGHT = true;
+    });
+    document.getElementById('button-right').addEventListener('touchend', (e)=>{
+        e.preventDefault();
+        keyboard.RIGHT = false;
+    });
+}
+
+
+function touchControlRightHand(){
+    document.getElementById('button-jump').addEventListener('touchstart', (e)=>{
+        e.preventDefault();
+        keyboard.SPACE = true;
+    });
+    document.getElementById('button-jump').addEventListener('touchend', (e)=>{
+        e.preventDefault();
+        keyboard.SPACE = false;
+    });
+    document.getElementById('button-attack').addEventListener('touchstart', (e)=>{
+        e.preventDefault();
+        keyboard.D = true;
+    });
+    document.getElementById('button-attack').addEventListener('touchend', (e)=>{
+        e.preventDefault();
+        keyboard.D = false;
+    });
+}
+
 
 window.addEventListener('keydown', (event) => {
-
     if (event.key == 'ArrowLeft') {
         keyboard.LEFT = true;
-
     }
-
     if (event.key == 'ArrowRight') {
         keyboard.RIGHT = true;
     }
-
-    if (event.key == 'ArrowUp') {
-        keyboard.UP = true;
-    }
-
-    if (event.key == 'ArrowDown') {
-        keyboard.DOWN = true;
-    }
-
     if (event.key == ' ') {
         keyboard.SPACE = true;
     }
-
     if (event.key == 'd') {
         keyboard.D = true;
     }
@@ -125,27 +153,15 @@ window.addEventListener('keydown', (event) => {
 
 
 window.addEventListener('keyup', (event) => {
-
     if (event.key == 'ArrowLeft') {
         keyboard.LEFT = false;
     }
-
     if (event.key == 'ArrowRight') {
         keyboard.RIGHT = false;
     }
-
-    if (event.key == 'ArrowUp') {
-        keyboard.UP = false;
-    }
-
-    if (event.key == 'ArrowDown') {
-        keyboard.DOWN = false;
-    }
-
     if (event.key == ' ') {
         keyboard.SPACE = false;
     }
-
     if (event.key == 'd') {
         keyboard.D = false;
     }
